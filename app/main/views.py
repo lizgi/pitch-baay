e1c12f0ed890e88fd087bbbcef4dec0c13722c4f
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-09 17:02:32 +0300

    add method

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template,request,redirect,url_for,abort, flash
+from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
-from ..models import Comment, User,Pitch
-from flask_login import login_required
-from .. import db,photos
+from ..models import Comment, User, Pitch, Upvote, Downvote
+from flask_login import login_required, current_user
+from .. import db, photos
 from .forms import UpdateProfile, PitchForm, CommentForm
 
 @main.route('/')
@@ -59,3 +59,93 @@ def add_comment():
         flash('Category added successfully.')
         return redirect(url_for('.index'))
     return render_template('add_category.html', form=form)
+
+@main.route('/post', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    post_form = PitchForm()
+    if post_form.validate_on_submit():
+        title = post_form.post_title.data
+        category = post_form.post_category.data
+        content = post_form.post_content.data
+        new_post = Pitch(title=title, content=content,
+                        user=current_user, category=category)
+        new_post.save_post()
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+
+    else:
+        all_posts = Pitch.query.order_by(Pitch.date_posted).all()
+
+    return render_template('pitches.html', posts=all_posts, post_form=post_form)
+
+
+@main.route('/post/<id>', methods=['GET', 'POST'])
+@login_required
+def post_details(id):
+    comments = Comment.query.filter_by(post_id=id).all()
+    posts = Pitch.query.get(id)
+    if posts is None:
+        abort(404)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(
+            comment=form.comment.data,
+            post_id=id,
+            user_id=current_user.id
+        )
+        db.session.add(comment)
+        db.session.commit()
+        form.comment.data = ''
+        flash('Your comment has been posted successfully!')
+    return render_template('comments.html', post=posts, comment=comments, comment_form=form)
+
+
+@main.route('/like/<int:id>', methods=['GET', 'POST'])
+@login_required
+def like(id):
+    post = Pitch.query.get(id)
+    if post is None:
+        abort(404)
+    like = Upvote.query.filter_by(user_id=current_user.id, post_id=id).first()
+    if like is not None:
+        db.session.delete(like)
+        db.session.commit()
+        flash('You have successfully unupvoted the pitch!')
+        return redirect(url_for('main.index'))
+    new_like = Upvote(
+        user_id=current_user.id,
+        post_id=id
+    )
+    db.session.add(new_like)
+    db.session.commit()
+    flash('You have successfully upvoted the pitch!')
+    return redirect(url_for('main.index'))
+
+
+@main.route('/dislike/<int:id>', methods=['GET', 'POST'])
+@login_required
+def dislike(id):
+    posts = Pitch.query.get(id)
+    if posts is None:
+        abort(404)
+    
+    dislike = Downvote.query.filter_by(
+        user_id=current_user.id, post_id=id).first()
+    if dislike is not None:
+       
+        db.session.delete(dislike)
+        db.session.commit()
+        flash('You have successfully undownvoted the pitch!')
+        return redirect(url_for('.index'))
+
+    new_dislike = Downvote(
+        user_id=current_user.id,
+        post_id=id
+    )
+    db.session.add(new_dislike)
+    db.session.commit()
+    flash('You have successfully downvoted the pitch!')
+    return redirect(url_for('.index')) 
+       
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
 
-from app.models import User, Comment, Pitch
+from app.models import User, Comment, Pitch,Category,Upvote,Downvote
 
 from flask_migrate import Migrate, MigrateCommand
 
@@ -24,9 +24,10 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment, Category = Category, Upvote = Upvote, Downvote = Downvote )
 
 
 if __name__ == '__main__':
     manager.run()
+    
 
