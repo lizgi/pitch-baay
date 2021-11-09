329daaf204833d7868ee1256498331ab81cca116
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-09 09:52:14 +0300

    a few changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,7 +16,7 @@ def login():
           login_user(user,login_form.remember.data)
           return redirect(request.args.get('next') or url_for('main.index'))
       flash('Invalid username or Password')
-  title = "Minute Post login"
+  title = "pitch login"
   return render_template('auth/login.html',login_form = login_form,title=title)
 @auth.route('/logout')
 @login_required
--- app/email.py
+++ app/email.py
@@ -2,9 +2,8 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 def mail_message(subject,template,to,**kwargs):
-    sender_email ='pitchbay@gmail.com'
+    sender_email ='pitchbay0@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
-    
