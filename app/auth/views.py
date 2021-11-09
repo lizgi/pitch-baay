d0519dea3b02de07123881bf1e11626a3200f10f
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-09 16:51:19 +0300

    add classes

--- app/auth/views.py
+++ app/auth/views.py
@@ -32,7 +32,8 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to Pitch-Bay","email/welcome_user",user.email,user=user)
+        # mail_message("Welcome to Pitch-Bay","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
--- app/models.py
+++ app/models.py
@@ -15,12 +15,12 @@ class Pitch(db.Model):
     title = db.Column(db.String(255))
     category = db.Column(db.String)
     comment = db.Column(db.Text)
-    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    date_pitched = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
     
 
-    def save_post(self):
+    def save_pitch(self):
         db.session.add(self)
         db.session.commit()
     def repr(self):
@@ -54,7 +54,7 @@ class Comment(db.Model):
 class User(UserMixin,db.Model):
     _tablename_ = 'users'
 
-    post = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
@@ -74,4 +74,62 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.pass_secure,password)
     def _repr_(self):
         return f'Users {self.username}'
+
+class Category(db.Model):
+    _tablename_ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    picture_path = db.Column(db.String(64))
+
+    @staticmethod
+    def get_all_category():
+        return Category.query.all()
+
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def _repr_(self):
+        return '<Category %r>' % self.name
+
+
+class Upvote(db.Model):
+    tablename = 'upvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitch.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls, id):
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
+        return upvote
+
+    def repr(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
+
+class Downvote(db.Model):
+    tablename = 'downvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitc_id = db.Column(db.Integer, db.ForeignKey('pitch.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_downvotes(cls, id):
+        downvote = Downvote.query.filter_by(pitch_id=id).all()
+        return downvote
+
+    def repr(self):
+        return f'{self.user_id}:{self.pitch_id}'        
         
