dc3aea23d59c37a9658fa48dff4a666adeb97f16
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-11-10 03:02:37 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Comment, Pitch,Category,Upvote,Downvote
 
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
